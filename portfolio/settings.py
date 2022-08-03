8e9bda9f9f8d53cc8cf97b98f1a0bd2d92e09c8f
FedeGM <gomezmonroyfederico@gmail.com>
2022-08-03 03:21:34 -0300

    Revert "minor change"
    This reverts commit 962a86621ab0ad8733654bad1ffe207d301de135.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-a(tx6q!*8sgzz4w9iw2m!dbdb&!8s5$(qlfh6xz##^v50+amd('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -3,4 +3,5 @@ from .models import Project, WorkExperience
 
 
 admin.site.register(Project)
+
 admin.site.register(WorkExperience)
